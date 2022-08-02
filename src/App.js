e89584e98741e95db4984b89409ddb688bb13738
AbijithS-aot <abijith.s@aot-technologies.com>
2022-08-02 13:09:35 +0530

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import EmailPage from './Signup/EmailPage';
 
 function App() {
   return (
     <div className="App">
-      helooo
+      <EmailPage />
     </div>
   );
 }
